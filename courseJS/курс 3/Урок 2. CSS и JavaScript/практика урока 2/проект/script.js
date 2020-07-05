31934548d414cb3440d658db6df1a05a717bfda5
Nastya <08322422nastya@gmail.com>
2020-07-05 20:37:58 +0300

    homework two lesson

---
+++ courseJS/курс 3/Урок 2. CSS и JavaScript/практика урока 2/проект/script.js
@@ -0,0 +1,26 @@
+// window.onload = function() {
+//     var btnToShowModal = document.querySelector('[value="Type a message"]');
+//     var modalWrapper = document.querySelector('#modal');
+//     var cancel = document.querySelector('[value="Cancel"]')
+
+
+//     btnToShowModal.onclick = toggleMessageWindow;
+//     cancel.onclick = toggleMessageWindow;
+
+//     function toggleMessageWindow() {
+//         modalWrapper.classList.toggle('modal__visible');
+//     }
+// }
+
+
+    var btnToShowModal = document.querySelector('[value="Type a message"]');
+    var modalWrapper = document.querySelector('#modal');
+    var cancel = document.querySelector('[value="Cancel"]')
+
+
+    btnToShowModal.onclick = toggleMessageWindow;
+    cancel.onclick = toggleMessageWindow;
+
+    function toggleMessageWindow() {
+        modalWrapper.classList.toggle('modal__visible');
+    }
